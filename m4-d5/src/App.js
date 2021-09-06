66a948167f74783124e03173ea6f4ebdabfdefc0
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-09-06 06:18:23 +0200

    completed

--- m4-d5/src/App.js
+++ m4-d5/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import CustomNavbar from './Component/Navbar'
+import Header from './Component/Header'
+import RowOfMovies from './Component/RowOfMovies'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CustomNavbar />
+      <Header />
+      <RowOfMovies title="Star Wars" />
+      <RowOfMovies title="Harry Potter" />
+      <RowOfMovies title="The lord of the rings" />
+    </>
+  )
 }
 
-export default App;
+export default App
